f1f2878cba569463abb18f6cd62f1919d53f7a44
Gabriel Eymat <geymat@z1r11p3.42lyon.fr>
2024-03-27 14:44:33 +0100

    segfault fixed, my bad

--- srcs/build_in/build_in_export_2.c
+++ srcs/build_in/build_in_export_2.c
@@ -6,7 +6,7 @@
 /*   By: lcamerly <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 21:43:06 by lcamerly          #+#    #+#             */
-/*   Updated: 2024/03/27 14:29:05 by geymat           ###   ########.fr       */
+/*   Updated: 2024/03/27 14:41:00 by geymat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,12 @@ int	do_export(char *temp, t_env **env)
 	key_value = sep_in_two(temp);
 	if (!key_value)
 		return (free_ret_1(temp));
-	replace_chars_in_str(key_value[1], -1, ' ');
-	rm_useless_quotes(key_value[1]);
+	if (key_value[1])
+	{
+		replace_chars_in_str(key_value[1], -1, ' ');
+		rm_useless_quotes(key_value[1]);
+	}
+	rm_useless_quotes(key_value[0]);
 	new_lst = ft_envlstnew_frees(key_value);
 	if (!new_lst)
 		return (free_ret_1(temp));
