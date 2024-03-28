c947a6a28a28f851051ad3810ebc4d9c6852294a
Gabriel Eymat <geymat@z3r11p5.42lyon.fr>
2024-03-28 13:31:30 +0100

    tabs OK

--- srcs/get_new_line.c
+++ srcs/get_new_line.c
@@ -6,7 +6,7 @@
 /*   By: lcamerly <lcamerly@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 05:13:05 by geymat            #+#    #+#             */
-/*   Updated: 2024/03/21 05:06:19 by geymat           ###   ########.fr       */
+/*   Updated: 2024/03/28 13:28:20 by geymat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,59 @@ static int	quotes(char *str)
 	return (delimiter);
 }
 
+static void	remove_dobble_spaces(char *line)
+{
+	size_t	i;
+	short	delimiter;
+
+	i = 0;
+	delimiter = 0;
+	while (line[i])
+	{
+		if (delimiter == 1 && line[i] == '\"')
+			delimiter = 0;
+		else if (delimiter == 2 && line[i] == '\'')
+			delimiter = 0;
+		else if (line[i] == '\"' && !delimiter)
+			delimiter = 1;
+		else if (line[i] == '\'' && !delimiter)
+			delimiter = 2;
+		if (!delimiter && line[i] == ' ' && line[i + 1] == ' ')
+		{
+			ft_strcpy(line + i, line + i + 1);
+			i--;
+		}
+		i++;
+	}
+
+}
+
+static void	replace_tabs_with_space(char *line)
+{
+	size_t	i;
+	short	delimiter;
+
+	if (!line)
+		return ;
+	i = 0;
+	delimiter = 0;
+	while (line[i])
+	{
+		if (delimiter == 1 && line[i] == '\"')
+			delimiter = 0;
+		else if (delimiter == 2 && line[i] == '\'')
+			delimiter = 0;
+		else if (line[i] == '\"' && !delimiter)
+			delimiter = 1;
+		else if (line[i] == '\'' && !delimiter)
+			delimiter = 2;
+		if (!delimiter && line[i] >= 9 && line[i] <= 13)
+			line[i] = ' ';
+		i++;
+	}
+	remove_dobble_spaces(line);
+}
+
 char	*get_a_new_line(t_env *env)
 {
 	char	*str;
@@ -59,5 +112,6 @@ char	*get_a_new_line(t_env *env)
 	str = ft_strjoin_free_first(str, "");
 	if (str && *str)
 		add_history(str);
+	replace_tabs_with_space(str);
 	return (replace_env(str, env));
 }
